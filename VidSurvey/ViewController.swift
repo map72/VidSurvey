08c63aee3621320888773881f616c048c1d80619
Periard, Misty <maperiaz@med.umich.edu>
2017-01-08 15:00:16 -0500

    Initial Commit

---
+++ VidSurvey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VidSurvey
+//
+//  Created by Periard, Misty on 1/8/17.
+//  Copyright © 2017 Periard, Misty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
